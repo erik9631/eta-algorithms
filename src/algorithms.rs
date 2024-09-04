0ea04ec2ea5c06259cb3c77e2150d524bef80ec0
erik9631 <erik9631@gmail.com>
2024-09-04 22:45:15 +0200

    feat: Added extract unique pairs

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,16 +1,17 @@
 use crate::data_structs::array::Array;
 use crate::data_structs::bitmap::Bitmap;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
-pub fn flat_diversity_score<T>(data: &[(i32, T)], hash_set: &mut HashMap<i32, usize>) -> (i32, Bitmap) {
-    let mut bitmap = Bitmap::new(data.len());
+pub fn calculate_uniqueness(identifiers: &[i32], indices_hash: &mut HashMap<i32, usize>) -> (i32, Bitmap) {
+    let mut bitmap = Bitmap::new(identifiers.len());
     let mut score = 0;
-    for (index, value) in data.iter().enumerate() {
-        if let Some(original_index) = hash_set.get(&value.0) {
+    for (index, value) in identifiers.iter().enumerate() {
+        if let Some(original_index) = indices_hash.get(value) {
             bitmap.set(*original_index, false);
             continue;
         }
-        hash_set.insert(value.0, index);
+        indices_hash.insert(*value, index);
         bitmap.set(index, true);
         score += 1;
     }
@@ -18,29 +19,56 @@ pub fn flat_diversity_score<T>(data: &[(i32, T)], hash_set: &mut HashMap<i32, us
     (score, bitmap)
 }
 
-pub fn best_flat_diversity<T>(data: &[(i32, T)], input_data: &[(i32, T)]) -> Array<(i32, T)>
-where
-    T: Copy + Sized,
-{
-    let mut top = input_data.len() - 1;
-    let mut hash_map = HashMap::with_capacity(data.len());
-    let (_, bitmap) = flat_diversity_score(data, &mut hash_map);
-    let mut new_data = Array::from_slice(data);
+pub fn optimize_diversity(existing_identifiers: &[i32], input_identifiers: &[i32]) -> Array<i32> {
+    let mut top = input_identifiers.len() - 1;
+    let mut hash_map = HashMap::with_capacity(existing_identifiers.len());
+    let (_, bitmap) = calculate_uniqueness(existing_identifiers, &mut hash_map);
+    let mut new_data = Array::from_slice(existing_identifiers);
     let accessible_indices = bitmap.to_indices_false();
     for accessible_index in accessible_indices {
         if top == 0 {
             break;
         }
 
-        let new_diversity = unsafe { input_data.get_unchecked(top) };
+        let new_diversity = unsafe { input_identifiers.get_unchecked(top) };
         top -= 1;
-        if hash_map.contains_key(&new_diversity.0) {
+        if hash_map.contains_key(new_diversity) {
             continue;
         }
-        new_data[accessible_index].0 = new_diversity.0;
-        new_data[accessible_index].1 = new_diversity.1;
+        new_data[accessible_index] = *new_diversity;
 
-        hash_map.insert(new_diversity.0, accessible_index);
+        hash_map.insert(*new_diversity, accessible_index);
     }
     new_data
+}
+
+pub fn extract_unique_pairs<T>(values: &[T], identifiers: &[i32]) -> (Vec<T>, Vec<i32>)
+where
+    T: Copy + Sized + Hash + Eq,
+{
+    let mut value_hashmap = HashMap::with_capacity(values.len());
+    let (_, unique_value_markers) = calculate_uniqueness(identifiers, &mut value_hashmap);
+    let value_uniques = unique_value_markers.to_indices_true();
+    let value_overlap = unique_value_markers.to_indices_false();
+    let mut output_values = Vec::with_capacity(value_uniques.len());
+    let mut output_indices = Vec::with_capacity(value_uniques.len());
+
+    for index in value_uniques.iter() {
+        output_values.push(unsafe { *values.get_unchecked(*index) });
+        output_indices.push(*index as i32);
+    }
+
+    // Now fill in the non-unique values, but only once!
+    let mut fill_out_hash = HashSet::with_capacity(value_overlap.len());
+    for index in value_overlap.iter() {
+        let value = unsafe { values.get_unchecked(*index) };
+        if fill_out_hash.contains(value) {
+            continue;
+        }
+        output_values.push(*value);
+        output_indices.push(*index as i32);
+        fill_out_hash.insert(value);
+    }
+
+    (output_values, output_indices)
 }
--- src/tests/algorithms.rs
+++ src/tests/algorithms.rs
@@ -1,21 +1,10 @@
-use crate::algorithms::{best_flat_diversity, flat_diversity_score};
+use crate::algorithms::{calculate_uniqueness, optimize_diversity};
 
 #[test]
 fn test_flat_diversity_score_diverse() {
-    let data = [
-        (1, ()),
-        (2, ()),
-        (3, ()),
-        (4, ()),
-        (5, ()),
-        (6, ()),
-        (7, ()),
-        (8, ()),
-        (9, ()),
-        (10, ()),
-    ];
+    let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut hash_map = std::collections::HashMap::with_capacity(data.len());
-    let (score, bitmap) = flat_diversity_score(&data, &mut hash_map);
+    let (score, bitmap) = calculate_uniqueness(&data, &mut hash_map);
     assert_eq!(score, 10);
     assert_eq!(bitmap.capacity(), 1);
     assert_eq!(bitmap.bit_capacity(), 10);
@@ -26,20 +15,9 @@ fn test_flat_diversity_score_diverse() {
 
 #[test]
 fn test_flat_diversity_score_equal() {
-    let data = [
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-        (1, ()),
-    ];
+    let data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     let mut hash_map = std::collections::HashMap::with_capacity(data.len());
-    let (score, bitmap) = flat_diversity_score(&data, &mut hash_map);
+    let (score, bitmap) = calculate_uniqueness(&data, &mut hash_map);
     assert_eq!(score, 1);
     assert_eq!(bitmap.capacity(), 1);
     assert_eq!(bitmap.bit_capacity(), data.len());
@@ -51,20 +29,9 @@ fn test_flat_diversity_score_equal() {
 
 #[test]
 fn test_flat_diversity_score_periodic() {
-    let data = [
-        (1, ()),
-        (2, ()),
-        (1, ()),
-        (2, ()),
-        (1, ()),
-        (2, ()),
-        (1, ()),
-        (2, ()),
-        (1, ()),
-        (2, ()),
-    ];
+    let data = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
     let mut hash_map = std::collections::HashMap::with_capacity(data.len());
-    let (score, bitmap) = flat_diversity_score(&data, &mut hash_map);
+    let (score, bitmap) = calculate_uniqueness(&data, &mut hash_map);
     assert_eq!(score, 2);
     assert_eq!(bitmap.capacity(), 1);
     assert_eq!(bitmap.bit_capacity(), data.len());
@@ -76,9 +43,9 @@ fn test_flat_diversity_score_periodic() {
 
 #[test]
 fn test_flat_diversity_score_diverse1() {
-    let data = [(1, ()), (3, ()), (3, ()), (4, ())];
+    let data = [1, 3, 3, 4];
     let mut hash_map = std::collections::HashMap::with_capacity(data.len());
-    let (score, bitmap) = flat_diversity_score(&data, &mut hash_map);
+    let (score, bitmap) = calculate_uniqueness(&data, &mut hash_map);
     assert_eq!(score, 3);
     assert_eq!(bitmap.capacity(), 1);
     assert_eq!(bitmap.bit_capacity(), data.len());
@@ -90,44 +57,32 @@ fn test_flat_diversity_score_diverse1() {
 
 #[test]
 fn test_best_flat_diversity_same() {
-    let data = [(1, ()), (1, ()), (1, ()), (1, ())];
-    let input_data = [(2, ()), (2, ()), (2, ()), (2, ())];
-    let new_data = best_flat_diversity(&data, &input_data);
-    assert_eq!(new_data[0].0, 2);
-    assert_eq!(new_data[1].0, 1);
-    assert_eq!(new_data[2].0, 1);
-    assert_eq!(new_data[3].0, 1);
+    let data = [1, 1, 1, 1];
+    let input_data = [2, 2, 2, 2];
+    let new_data = optimize_diversity(&data, &input_data);
+    assert_eq!(new_data[0], 2);
+    assert_eq!(new_data[1], 1);
+    assert_eq!(new_data[2], 1);
+    assert_eq!(new_data[3], 1);
 }
 
 #[test]
 fn test_best_flat_diversity_diverge_same() {
-    let data = [(1, ()), (2, ()), (1, ()), (2, ())];
-    let input_data = [(1, ()), (2, ()), (1, ()), (2, ())];
-    let new_data = best_flat_diversity(&data, &input_data);
-    assert_eq!(new_data[0].0, 1);
-    assert_eq!(new_data[1].0, 2);
-    assert_eq!(new_data[2].0, 1);
-    assert_eq!(new_data[3].0, 2);
+    let data = [1, 2, 1, 2];
+    let input_data = [1, 2, 1, 2];
+    let new_data = optimize_diversity(&data, &input_data);
+    assert_eq!(new_data[0], 1);
+    assert_eq!(new_data[1], 2);
+    assert_eq!(new_data[2], 1);
+    assert_eq!(new_data[3], 2);
 }
 #[test]
 fn test_best_flat_diversity_diverge_change() {
-    let data = [(1, ()), (2, ()), (1, ()), (2, ())];
-    let input_data = [(1, ()), (2, ()), (1, ()), (3, ())];
-    let new_data = best_flat_diversity(&data, &input_data);
-    assert_eq!(new_data[0].0, 3);
-    assert_eq!(new_data[1].0, 2);
-    assert_eq!(new_data[2].0, 1);
-    assert_eq!(new_data[3].0, 2);
-}
-
-#[test]
-fn test_best_flat_diversity_diverge_different_size() {
-    let data = [(1, 1), (1, 2), (1, 4), (1, 8)];
-    let input_data = [(1, 2), (1, 4), (1, 8), (2, 2), (2, 4), (2, 16), (3, 8)];
-    let new_data = best_flat_diversity(&data, &input_data);
-
-    assert_eq!(new_data[0], (3, 8));
-    assert_eq!(new_data[1], (2, 16));
-    assert_eq!(new_data[2], (1, 4));
-    assert_eq!(new_data[3], (1, 8));
+    let data = [1, 2, 1, 2];
+    let input_data = [1, 2, 1, 3];
+    let new_data = optimize_diversity(&data, &input_data);
+    assert_eq!(new_data[0], 3);
+    assert_eq!(new_data[1], 2);
+    assert_eq!(new_data[2], 1);
+    assert_eq!(new_data[3], 2);
 }
